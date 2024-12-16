21e2900a117ed9911cb22ad4608d756184296a07
CpmJhon21 <cpmjho@gmail>
2024-12-16 10:31:11 +0700

    Welcome To Cpm_Jhon21

---
+++ links.js
@@ -0,0 +1,51 @@
+export const links = [
+    {
+        name: "Donate Saweria",
+        link: 
+"https://saweria.co/CpmJhon",
+        image: 
+"saweria.png",
+    },
+    {
+        name: "Donate PayPal",
+        link: 
+"https://www.paypal.me/cpmjhon",
+        image: 
+"pay.png",
+    },
+    {
+        name: "WhatsApp",
+        link: 
+"https://wa.me/6285894955362",
+        image: 
+"whatapps.png",
+    },
+    {
+        name: "YouTube",
+        link: 
+"https://youtube.com/@galeri_car_cpm8048",
+        image: 
+"youtube.png",
+    },
+    {
+        name: "Instagram",
+        link: 
+"https://instagram.com/cpm_jhon?igshid=YmMyMTA2M2Y=",
+        image: 
+"instagram.png",
+    },
+    {
+        name: "TikTok",
+        link: 
+"https://tiktok.com/@garasidriftcpm",
+        image: 
+"tiktok.png",
+    },
+    {
+        name: "Website",
+        link: 
+"https://cpmjhon.github.io/Tes/",
+        image: 
+"globe.png",
+    },
+];
